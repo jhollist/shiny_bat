9642086fb11d8ac81f677ca743ead46b1874437d
Jeff Hollister <jeff.w.hollister@gmail.com>
2021-11-18 09:52:34 -0500

    update bat

--- shiny_rlocal.bat
+++ shiny_rlocal.bat
@@ -1,2 +1,3 @@
 "C:/PROGRA~1/R/R-41~1.0/bin/R.exe" -e "shiny::runApp('app.R', launch.browser = TRUE)"
-
+pause
+:: pause is in here to let you see any errors
--- shiny_rportable.bat
+++ shiny_rportable.bat
@@ -1,2 +1,3 @@
-"R-Portable/App/R-Portable/bin/R.exe" -e "shiny::runApp('../app.R', launch.browser = TRUE)"
-
+"R-Portable/App/R-Portable/bin/R.exe" -e "shiny::runApp('app.R', launch.browser = TRUE)"
+pause
+:: pause is in here to let you see any errors
